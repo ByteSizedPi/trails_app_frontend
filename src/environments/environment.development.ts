f7f5bd5ade2890304ca5237f981dac73a352acb4
Johan Venter <johann12venter@gmail.com>
2024-11-25 14:28:29 +0200

    empty event state added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,6 @@ export type ENV = {
 };
 
 export const environment: ENV = {
-  BASE_URL: "http://localhost:3000/api/",
+  // BASE_URL: "http://localhost:3000/api/",
+  BASE_URL: "https://trialsobserver.co.za/api/",
 };
